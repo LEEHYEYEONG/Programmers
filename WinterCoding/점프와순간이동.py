210899368bb798e7164e0f38b7ce621b39c8bd92
LEEHYEYEONG <elto789@naver.com>
2023-10-26 14:21:01 +0900

    점프와순간이동

--- WinterCoding/점프와순간이동.py
+++ WinterCoding/점프와순간이동.py
@@ -1,20 +1,38 @@
+# n_list = [2**i for i in range(31)] 
+
+# def add(x):
+#     return x+1
+
+# def solution(n):
+#     ans = 1
+#     result_list = [1, 2, 2, 3]
+#     if(n == 1 or n in n_list):
+#         ans = 1
+#     elif(n == 3):
+#         ans = 2
+#     else:
+#         for i in range(2, 31):
+#             if(2**i < n < 2**(i+1)):
+#                 result_i = i
+#                 break
+#         for i in range(3, result_i+2):
+#             new_result_list = list(map(add, result_list))
+#             result_list.extend(new_result_list)			
+               
+#         ans = result_list[n-2**result_i]
+
+#     return ans
+
 def solution(n):
-    ans = 1
-    distance = 1
+    ans = 0
+    if(n == 1):
+        return ans + 1
 
-
-    while True:
-        if(n - distance * 2 > 0):
-            #n -= distance * 2
-            distance *= 2
-        elif(n - distance * 2 <= 0):
-            #distance += n - distance
-            ans += n - distance 
-            distance += n - distance
-            break
-
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-
+    while (n != 0):
+        if(n % 2 == 0):
+            n = n//2
+        else:
+            n = (n-1) // 2
+            ans += 1
+   
     return ans
-
-print(solution(5000))
