8b11e5b13a41c25c2f8e1c46bcf2829b7c9ebb04
leehyeyeong <elto789@naver.com>
2023-11-23 15:12:15 +0900

    풀이 수정

--- Lv.1/약수의개수와덧셈.py
+++ Lv.1/약수의개수와덧셈.py
@@ -1,5 +1,15 @@
+# def solution(left, right):
+#     divisor_list = [i if len([j for j in range(1, i+1) if i % j == 0]) % 2 == 0 else -i for i in range(left, right+1)]
+#     return sum(divisor_list)
+
+# 제곱수의 약수의 개수가 홀수 임을 이용한 풀이
 def solution(left, right):
-    divisor_list = [i if len([j for j in range(1, i+1) if i % j == 0]) % 2 == 0 else -i for i in range(left, right+1)]
-    return sum(divisor_list)
+    answer = 0
+    for i in range(left,right+1):
+        if int(i**0.5)==i**0.5:
+            answer -= i
+        else:
+            answer += i
+    return answer
 
 print(solution(24, 27))
